d4f3c080cd08b03568719dfa915465eb509e368f
Mai Pham <maitphm@gmail.com>
2020-11-13 20:02:57 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -6,10 +6,10 @@ def find_item_by_name_in_collection(name, collection)
   while count < collection.length 
     if collection[count][:item] == name 
       return collection[count]
-    end 
-  count += 1
+    end
+    count += 1
+  end 
 end 
-end
 
 def consolidate_cart(cart)
   
